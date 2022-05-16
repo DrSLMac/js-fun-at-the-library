38369d4c637d00b800e81cbf7310d9308eb0f049
Shauna MacFarlane <shauna.macfarlane579@gmail.com>
2022-05-15 22:15:18 -0500

    incomplete

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,17 @@
+function createLibrary(denverLibrary) {
+var libraryInfo = {
+  name: "Denver Public Library",
+}
+return libraryInfo.name
+}
+// next step is to name the library
+//parameter would be libraryName
+//function createLibrary(libraryName) {
+// dot notation indicates an object.
 
 
 module.exports = {
-  // createLibrary,
+  createLibrary,
   // addBook,
   // checkoutBook
 };
